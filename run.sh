32ba40ae61a50154ea8fc1172a849eb91d561b84
nobody <nobody@example.com>
2016-12-21 11:27:32 -0500

    fixes

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-source .config
+set -o verbose   # show commands as they are executed
+set -o xtrace    # expand variables
+
+MAILCMD=`which mail`
+
+if [[ -f "${HOME}/.steem_price_feed_bash/.config" ]]; then
+	source $HOME/.steem_price_feed_bash/.config
+else
+	echo "You need to set up a .config - see README.md"
+	exit 137
+fi
 
 until ./steem_price_feed.bash; do
     MESSAGE="Server './steem_price_feed.bash' crashed with exit code $?.  Respawning.."
-    echo $MESSAGE >&2
-    if [ ${PRICE_EMAIL:-} ]; then
-        echo $MESSAGE | mail -s ${PRICE_EMAIL}
+    echo ${MESSAGE} >&2
+    if [  -n "${PRICE_EMAIL}" ]; then
+        echo ${MESSAGE} | ${MAILCMD} -s "${MESSAGE}" ${PRICE_EMAIL}
     fi
-    sleep 1
+    sleep 60
 done
